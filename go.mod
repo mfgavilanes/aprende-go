b81de2fe11244bb9110e1ec3d23fbed0d9cfcd78
mfgavilanes <mfgavilanes@esei.uvigo.es>
2026-03-05 10:28:59 +0100

    Version1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mfgavilanes/aprende-go
+
+go 1.25.7
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-import "github.com/rs/zerolog/log"
+//import "github.com/rs/zerolog/log"
 
 func main() {
-	log.Info().Msg("Hola")
+	//log.Info().Msg("Hola")
 	fmt.Println("hola")
 }
